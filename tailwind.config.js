e576a664c2d31b8e42535a144b26f2ef8c7ced64
A. J <117919468+adnj27@users.noreply.github.com>
2023-05-07 11:49:56 -0400

    feat: Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 /* eslint-disable-next-line no-undef */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
