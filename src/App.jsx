f7d91e8f0dc59d7c81e2aad3f01a2c6e169ba4d8
FOWMind <alwl1@hotmail.com>
2023-05-08 10:50:19 -0300

    feat(layers): :lipstick: implement responsive navbar and base layer background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { Layer, Navbar } from './components'
 
 function App() {
   return (
-    <>
-      <div>H</div>
-    </>
+    <Layer>
+      <Navbar />
+    </Layer>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,12 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant('child', '& *')
+      addVariant('child-hover', '& *:hover')
+      addVariant('direct-child', '& > *')
+      addVariant('direct-child-hover', '& > *:hover')
+    },
+  ],
 }
