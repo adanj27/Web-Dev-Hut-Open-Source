b8c16cc9b141d67985e8428574d58a03c228450b
Adan. J <117919468+adanj27@users.noreply.github.com>
2023-05-10 19:33:55 -0400

    feat(hero): add hero section (#31)
    * feat(hero): add hero-section
    
    * feat(hero): add her-section
    
    ---------
    
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>

--- src/components/layers/Layer.jsx
+++ src/components/layers/Layer.jsx
@@ -2,7 +2,7 @@ import { Navbar, Footer } from '../'
 
 export function Layer({ children }) {
   return (
-    <div className="bg-[#201F47]">
+    <div className="bg-[#201F47] bg-hero-bg bg-no-repeat bg-cover">
       <div className="px-5 py-10 xl:px-20 xl:py-14">
         <Navbar />
         <div className="relative z-20 min-h-screen">{children}</div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import { Hero } from '../components/Home/Hero'
 
 export const Home = () => {
-  return <div>Home</div>
+  return (
+    <>
+      <Hero />
+    </>
+  )
 }
