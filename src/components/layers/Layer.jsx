4494c01c961f6265c1a1036cdfe0e23998861ae3
FOWMind <alwl1@hotmail.com>
2023-05-10 11:17:36 -0300

    style(layers): :lipstick: correct z-index and margin for layers

--- src/components/layers/Layer.jsx
+++ src/components/layers/Layer.jsx
@@ -3,7 +3,7 @@ import { Navbar, Footer } from '../'
 export function Layer({ children }) {
   return (
     <div className="bg-[#201F47]">
-      <div className="px-5 py-10 xl:px-20 xl:py-14">
+      <div className="relative z-20 px-5 py-10 xl:px-20 xl:py-14">
         <Navbar />
         <div className="min-h-screen">{children}</div>
       </div>
