a16b86b77698095455124fd761edd1e3f6dd1378
Mind <alwl1@hotmail.com>
2023-05-10 12:02:50 -0300

    style(layers): :lipstick: layer z-index/classes correction (#29)

--- src/components/layers/Layer.jsx
+++ src/components/layers/Layer.jsx
@@ -3,9 +3,9 @@ import { Navbar, Footer } from '../'
 export function Layer({ children }) {
   return (
     <div className="bg-[#201F47]">
-      <div className="relative z-20 px-5 py-10 xl:px-20 xl:py-14">
+      <div className="px-5 py-10 xl:px-20 xl:py-14">
         <Navbar />
-        <div className="min-h-screen">{children}</div>
+        <div className="relative z-20 min-h-screen">{children}</div>
       </div>
       <Footer />
     </div>
