e4c926091dc1838763582d83ce4ac64526991e83
ykIsCoding <64340151+ykIsCoding@users.noreply.github.com>
2023-05-16 02:09:04 +0800

    solo he cambiado index.html y pagina de rutas despues llorando (#51)
    * update: logo html (#9)
    
    * update: readme
    
    * update: readme
    
    * update: readme
    
    * update: add logo
    
    ---------
    
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>
    
    * Add Navbar and Footer, install React-Icons (#13)
    
    * update: readme
    
    * update: readme
    
    * update: readme
    
    * update: add logo
    
    * feat(layers): :lipstick: implement responsive navbar and base layer background (#10)
    
    * update: logo html (#9)
    
    * update: readme
    
    * update: readme
    
    * update: readme
    
    * update: add logo
    
    ---------
    
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>
    
    * feat(layers): :lipstick: implement responsive navbar and base layer background
    
    ---------
    
    Co-authored-by: Adan. J <117919468+adanj27@users.noreply.github.com>
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>
    
    * feat(layers): 💄 implement responsive footer (#11)
    
    * update: logo html (#9)
    
    * update: readme
    
    * update: readme
    
    * update: readme
    
    * update: add logo
    
    ---------
    
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>
    
    * feat(layers): :lipstick: implement responsive footer
    
    ---------
    
    Co-authored-by: Adan. J <117919468+adanj27@users.noreply.github.com>
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>
    
    * update: readme (#12)
    
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>
    Co-authored-by: Mind <alwl1@hotmail.com>
    
    * he agregado pagina de rutas
    
    ---------
    
    Co-authored-by: Adan. J <117919468+adanj27@users.noreply.github.com>
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>
    Co-authored-by: Mind <alwl1@hotmail.com>

--- src/pages/LearnRoutes.jsx
+++ src/pages/LearnRoutes.jsx
@@ -1,5 +1,43 @@
-import React from 'react'
+import { Component, React } from 'react'
+import { IconButton } from '../components'
 
-export const LearnRoutes = () => {
-  return <div>LearnRoutes</div>
+class LearnRoutes extends Component {
+  static routename = '/rutas'
+  render() {
+    return (
+      <div class="text-white flex-col my-10">
+        <div class="flex">
+          <div class="flex-1 grid gap-y-3">
+            <h1 class="font-thin text-sm">
+              ¿No está seguro de qué camino tomar?
+            </h1>
+            <break />
+            <h1 class="font-bold text-xl">
+              Rutas de <br /> aprendizaje
+            </h1>
+            <break />
+            <h1 class="font-medium text-lg">
+              Aquí presentamos dos rutas de
+              <br /> aprendizaje populares para el
+              <br /> desarrollo web.
+            </h1>
+          </div>
+          <div class="grid flex-1">
+            <div class="flex space-x-5 justify-center">
+            <button class="rounded-full">1</button>
+          <button class="rounded-full">2</button>
+          <button class="rounded-full">3</button>
+            </div>
+          </div>
+          
+        </div>
+        <div class="flex grid-cols-2 gap-5 justify-center">
+          <button class="rounded-full">FrontEnd</button>
+          <button class="rounded-full">BackEnd</button>
+        </div>
+      </div>
+    )
+  }
 }
+
+export default LearnRoutes
