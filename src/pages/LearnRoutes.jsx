e0f0f35feb3cea2f690c80439410abcb793407b7
ykIsCoding <64340151+ykIsCoding@users.noreply.github.com>
2023-05-17 11:44:07 +0800

    Completado routes page (#53)
    * update: logo html (#9)
    
    * update: readme
    
    * update: readme
    
    * update: readme
    
    * update: add logo
    
    ---------
    
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>
    
    * Add Navbar and Footer, install React-Icons (#13)
    
    * update: readme
    
    * update: readme
    
    * update: readme
    
    * update: add logo
    
    * feat(layers): :lipstick: implement responsive navbar and base layer background (#10)
    
    * update: logo html (#9)
    
    * update: readme
    
    * update: readme
    
    * update: readme
    
    * update: add logo
    
    ---------
    
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>
    
    * feat(layers): :lipstick: implement responsive navbar and base layer background
    
    ---------
    
    Co-authored-by: Adan. J <117919468+adanj27@users.noreply.github.com>
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>
    
    * feat(layers): 💄 implement responsive footer (#11)
    
    * update: logo html (#9)
    
    * update: readme
    
    * update: readme
    
    * update: readme
    
    * update: add logo
    
    ---------
    
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>
    
    * feat(layers): :lipstick: implement responsive footer
    
    ---------
    
    Co-authored-by: Adan. J <117919468+adanj27@users.noreply.github.com>
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>
    
    * update: readme (#12)
    
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>
    Co-authored-by: Mind <alwl1@hotmail.com>
    
    * finished routes page
    
    * Update App.jsx
    
    ---------
    
    Co-authored-by: Adan. J <117919468+adanj27@users.noreply.github.com>
    Co-authored-by: A. J <117919468+adnj27@users.noreply.github.com>
    Co-authored-by: Mind <alwl1@hotmail.com>

--- src/pages/LearnRoutes.jsx
+++ src/pages/LearnRoutes.jsx
@@ -1,42 +1,92 @@
-import { Component, React } from 'react'
+import { Component, React, useState } from 'react'
+import { Button, Container, SearchField } from '../components'
 import { IconButton } from '../components'
 
-class LearnRoutes extends Component {
-  static routename = '/rutas'
-  render() {
+
+import html5Icon from '../assets/icons/html5.svg'
+import css3Icon from '../assets/icons/css3.svg'
+import javascriptIcon from '../assets/icons/javascript.svg'
+import typescriptIcon from '../assets/icons/typescript.svg'
+import angularIcon from '../assets/icons/angular.svg'
+import reactjsIcon from '../assets/icons/reactjs.svg'
+import nextjsIcon from '../assets/icons/nextjs.svg'
+import nodejsIcon from '../assets/icons/nodejs.svg'
+import javaIcon from '../assets/icons/java.svg'
+import pythonIcon from '../assets/icons/python.svg'
+import rubyIcon from '../assets/icons/ruby.svg'
+import sqlIcon from '../assets/icons/sql.svg'
+import dockerIcon from '../assets/icons/docker.svg'
+
+
+export default function LearnRoutes() {
+    const imgStyle = "rounded-lg z-50 my-20 shadow-md min-w-full min-h-full"
+    const [imgIndex, setImgIndex] = useState(0);
     return (
-      <div class="text-white flex-col my-10">
-        <div class="flex">
-          <div class="flex-1 grid gap-y-3">
-            <h1 class="font-thin text-sm">
-              ¿No está seguro de qué camino tomar?
-            </h1>
-            <break />
-            <h1 class="font-bold text-xl">
-              Rutas de <br /> aprendizaje
-            </h1>
-            <break />
-            <h1 class="font-medium text-lg">
-              Aquí presentamos dos rutas de
-              <br /> aprendizaje populares para el
-              <br /> desarrollo web.
-            </h1>
-          </div>
-          <div class="grid flex-1">
-            <div class="flex space-x-5 justify-center">
-              <button class="rounded-full">1</button>
-              <button class="rounded-full">2</button>
-              <button class="rounded-full">3</button>
+      <div className="mx-auto">
+        <Container className="relative z-10">
+      <div className="text-white flex-col my-10">
+        <div className="grid grid-cols-2">
+
+        <div className="text-white">
+        <p className="mb-3 sm:text-sm text-xs">
+          ¿No estás seguro de qué camino tomar?
+        </p>
+        <h2 className="font-bold sm:text-5xl text-3xl max-w-xs">
+          Rutas de aprendizaje
+        </h2>
+        <p className="my-3 sm:text-lg text-md max-w-[245px]">
+          Aquí presentamos dos rutas de aprendizaje populares para el desarrollo
+          web.
+        </p>
+      </div>
+
+          <div className="grid flex-1">
+            <div className="flex justify-end">
+             <ul className="my-10 direct-child:inline-block direct-child:bg-gradient-to-b direct-child:from-white direct-child:to-gray-500 direct-child:p-[0.4rem] direct-child:rounded-[50px] sm:direct-child:mr-6 direct-child:mr-3 [&_img]:w-6 [&_img]:h-6 [&_img]:object-contain [&_img]:select-none [&_img]:pointer-events-none">
+        <li>
+          <img src={html5Icon} draggable="false" />
+        </li>
+        <li>
+          <img src={css3Icon} draggable="false" />
+        </li>
+        <li>
+          <img src={javascriptIcon} draggable="false" />
+        </li>
+        <li>
+          <img src={reactjsIcon} draggable="false" />
+        </li>
+        <li>
+          <img src={javaIcon} draggable="false" />
+        </li>
+        <li>
+          <img src={pythonIcon} draggable="false" />
+        </li>
+        <li>
+          <img src={rubyIcon} draggable="false" />
+        </li>
+      </ul>
+            </div>
+            <div className="overflow-hidden mx-6">
+              <SearchField className="float-right" />
+
             </div>
           </div>
         </div>
-        <div class="flex grid-cols-2 gap-5 justify-center">
-          <button class="rounded-full">FrontEnd</button>
-          <button class="rounded-full">BackEnd</button>
+        <div className="flex grid-cols-2 gap-10 justify-center my-5">
+          <Button onClick={()=>setImgIndex(0)}>FrontEnd</Button>
+          <Button onClick={()=>setImgIndex(1)}>BackEnd</Button>
         </div>
       </div>
+      <div className="flex justify-center">
+        
+      {imgIndex==0?<img className={imgStyle} src='src\assets\frontend.png' alt="foto de ruta"/>:<img className={imgStyle} src='src\assets\backend.png' alt="foto de ruta"/>}
+        
+      </div>
+      </Container>
+      </div>
     )
-  }
 }
 
-export default LearnRoutes
+
+
+
