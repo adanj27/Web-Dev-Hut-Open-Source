81f6819c045220773c38cb4fdd1fa4fbcfa19ccc
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2023-05-30 12:33:26 -0600

    feat:Allow users to scroll to the top page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { Layer } from './components'
 import { AppRouter } from './router/AppRouter'
+import ScrollButton from './components/ScrollButton/ScrollButton'
 
 function App() {
   return (
     <BrowserRouter>
       <Layer>
         <AppRouter />
+        <ScrollButton />
       </Layer>
     </BrowserRouter>
   )
