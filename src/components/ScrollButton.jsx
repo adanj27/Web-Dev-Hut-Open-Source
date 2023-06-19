2384180ba48974d5bb3537529ae2e2dafe7a3ae3
Mind <alwl1@hotmail.com>
2023-06-19 10:35:43 -0300

    refactor: scroll button now made with tailwind (#72)
    * chore: :lock: dependency update due to 1 vulnerability
    
    * feat: :sparkles: integrate 404 page
    
    * feat: integrate contributors api request
    
    * fix(code): unused semicolon
    
    * fix: pages images
    
    * refactor: scroll button now made with tailwind

---
+++ src/components/ScrollButton.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+export const ScrollButton = () => {
+  const [visible, setVisible] = useState(false)
+  const scrollOffset = 200
+
+  const handleClick = () => {
+    window.scroll({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
+  document.addEventListener('scroll', () => {
+    const scrollToTop = document.documentElement.scrollTop
+    scrollToTop > scrollOffset ? setVisible(true) : setVisible(false)
+  })
+
+  return (
+    <button
+      className={
+        visible
+          ? 'border-none flex cursor-pointer fixed bottom-[1rem] right-[1rem] z-[9999] bg-white rounded-3xl p-2 text-[#2738a4] shadow-md transition transition-duration-300 motion-reduce:transition-none'
+          : null
+      }
+      onClick={handleClick}
+    >
+      <svg
+        viewBox="0 0 24 24"
+        className="stroke-current fill-none w-[24px] stroke-width-[3px]"
+      >
+        <path d="m4 16 8-8 8 8"></path>
+      </svg>
+    </button>
+  )
+}
