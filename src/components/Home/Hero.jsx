0125ce1641a358ecc0bd195e03bca64f0e85371f
A. J <117919468+adnj27@users.noreply.github.com>
2023-06-19 17:43:32 -0400

    update: home links and responsive about

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -33,7 +33,9 @@ export const Hero = () => {
             </Paragraph>
             <button className="sm:w-[250px] w-[180px] bg-gradient-to-b from-[#ffffff] to-[#909090] py-2 px-5 rounded-[15px] flex items-center sm:text-xl text-lg font-semibold">
               <BsDiscord className="text-5xl text-[#3D3D3D]" />
-              <span className="leading-6">Comunidad de discord</span>
+              <a href="https://discord.gg/DCrVhFydDW" className="leading-6">
+                Comunidad de discord
+              </a>
             </button>
             <Paragraph className="m:text-lg my-5 w-1/2">
               Únete a nuestra comunidad de discord para informarte de todas las
