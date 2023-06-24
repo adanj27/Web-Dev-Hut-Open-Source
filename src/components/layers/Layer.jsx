f42a518f57e818d7606435ffd999fb23c95c9379
Mind <alwl1@hotmail.com>
2023-06-24 05:33:13 -0300

    style(nav, footer): white space and nav logo in middle corrections (#80)

--- src/components/layers/Layer.jsx
+++ src/components/layers/Layer.jsx
@@ -2,10 +2,12 @@ import { Navbar, Footer } from '../'
 
 export function Layer({ children }) {
   return (
-    <div className="bg-[#201F47] relative overflow-x-hidden">
+    <div className="bg-[#201F47] relative overflow-x-hidden min-h-screen flex flex-col">
       <Navbar />
-      {children}
-      <Footer />
+      <div className="grow">
+        {children}
+      </div>
+      <Footer className="justify-self-end" />
     </div>
   )
 }
