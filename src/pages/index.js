1f74d95220ec27b94075886f6135dde22dea0d1e
Mind <alwl1@hotmail.com>
2023-07-01 09:12:05 -0300

    fix: remove spaces

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,6 @@ export * from './Frontend'
 export * from './Backend'
 export * from './Other'
 export * from './Error404'
-
-
 export * from './other'
-
 export * from './Technology'
 export * from './Guide'
