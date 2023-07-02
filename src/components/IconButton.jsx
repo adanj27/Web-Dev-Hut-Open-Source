909655c726d4f963edc721a40bebfceb590884c0
Mind <alwl1@hotmail.com>
2023-07-02 08:25:08 -0300

    feat(guide code): code copy feature (#94)
    * style(buttons): design corrections
    
    * feat(guide code): code copy feature

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,8 +1,10 @@
-export function IconButton({ Icon, as = 'button', ...props }) {
+export function IconButton({ icon, as = 'button', className = '', ...props }) {
   const As = as
+  const Icon = icon
+
   return (
-    <As className="child:w-10 child:h-10" {...props}>
-      <Icon className="px-2" />
+    <As className={`child:w-10 child:h-10 ${className}`} {...props}>
+      {icon && <Icon className="px-2 text-white transition" />}
     </As>
   )
 }
